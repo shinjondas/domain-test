63253c5ed7b92b0bc33850c1ec35431e46c952c4
Suvigya Saxena <suvigya2001@gmail.com>
2021-07-05 16:23:11 +0530

    Added gitignore, mongodb atlas uri, added Procfile as well

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
-
-mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true });
+// mongo_uri="mongodb+srv://admin-ieeecas:ieeecasmongodb@cluster0.yozy1.mongodb.net/test?retryWrites=true&w=majority"
+mongoose.connect("mongodb+srv://admin-ieeecas:ieeecasmongodb@cluster0.yozy1.mongodb.net/test?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.connection.once("open", () => {
   console.log("connected to MONGO");
 });
@@ -94,7 +94,8 @@ app.post("/test/domain",(req,res)=>{
 app.get("/test/invalid",(req,res)=>{
     res.render("invalid");
 })
-app.listen(3000, function() {
-    console.log("Server started on port 3000");
+const port = process.env.PORT || 3000;
+app.listen(port, function() {
+    console.log("Server started successfully");
   });
   

525ece817deb485eb4472165cad6767f77a3f572
shinjondas <shinjon12@gmail.com>
2021-07-23 20:29:53 +0530

    Updates

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-        var countDownDate = new Date("July 16, 2021 11:00:00").getTime();
+        var countDownDate = new Date("July 23, 2021 23:30:00").getTime();
         var x = setInterval(function(){
         var now = new Date().getTime();
         var distance =countDownDate-now;

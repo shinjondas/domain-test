56f293c24e3b83208f2eec10c00dc5ad72db40f9
shinjondas <shinjon12@gmail.com>
2021-07-27 12:58:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -134,7 +134,7 @@ app.get("/test/domain", (req, res) => {
     var now = new Date().getTime();
     var activ= new Date("July 27, 2021 12:48:00").getTime();
     console.log(now);
-    console.log(active);
+    console.log(activ);
 
     if(now >= activ){
         res.render("domtest", {
